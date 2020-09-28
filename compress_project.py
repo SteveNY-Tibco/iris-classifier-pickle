634be8e9af1210d9ec8d78c372fbf7c46d5578e3
Steven Yang <steven.yang@tibco.com>
2020-09-28 16:00:26 -0400

    Update compress_project.py

--- compress_project.py
+++ compress_project.py
@@ -1,13 +1,4 @@
 from tools import bash
 
 def main():
-  command = ''
-  counter = 0
-  for commandElement in sys.argv:
-    if i!=0
-      if i>1
-        command += ' ' + commandElement
-      else
-        command += commandElement
-    counter++
-  bash(command)
+  bash('tar -zcvf newrelic.tar.gz ./)
