4c28aed7e765d1a104ef1c6d64ffc9048b2b85d0
Steven Yang <steven.yang@tibco.com>
2020-09-28 15:56:58 -0400

    Create compress_project.py

---
+++ compress_project.py
@@ -0,0 +1,13 @@
+from tools import bash
+
+def main():
+  command = ''
+  counter = 0
+  for commandElement in sys.argv:
+    if i!=0
+      if i>1
+        command += ' ' + commandElement
+      else
+        command += commandElement
+    counter++
+  bash(command)
