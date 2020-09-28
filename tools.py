3368479ab73d35410b1ca9a2bcf98290aa57b29f
Steven Yang <steven.yang@tibco.com>
2020-09-28 16:17:15 -0400

    Update tools.py

--- tools.py
+++ tools.py
@@ -3,4 +3,4 @@ import subprocess
 def bash(command):
     process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
-    return output
+    return output, error
