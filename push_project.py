b60c52a22c3e5c99dcdae8fabee44d326c111ac3
Steven Yang <steven.yang@tibco.com>
2020-09-28 16:59:51 -0400

    Update push_project.py

--- push_project.py
+++ push_project.py
@@ -1,4 +1,10 @@
 from tools import bash
 
 def main():
-  bash('curl -X POST -H "Content-Type: binary/octet-stream" -d @./newrelic.tar.gz http://127.0.0.1:10091/mops/projectmgr/pushProject/newrelic.tar.gz')
+  try:
+    bash('curl -X POST -H "Content-Type: binary/octet-stream" -d @./newrelic.tar.gz http://127.0.0.1:10091/mops/projectmgr/pushProject/newrelic.tar.gz')
+    return 0
+  except Exception, err:
+    return 1
+  finally:
+    print 'Finally: cleanup'
